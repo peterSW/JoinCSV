f14330f536c641976f79a54b8d3e355cf72518cd
Peter Stroia-Williams <peterDwilliams@gmail.com>
2014-01-11 21:18:15 +0000

    Stops tkjoincsv.py from outputting extra lines.

--- src/tkjoincsv.py
+++ src/tkjoincsv.py
@@ -4,8 +4,8 @@ import joincsv
 
 if __name__ == '__main__':
     filetypes=[("csv", "*.csv")]
-    inputFile = tkFileDialog.askopenfile(filetypes=filetypes)
-    outputFile = tkFileDialog.asksaveasfile(filetypes=filetypes, defaultextension=".csv")
+    inputFile = tkFileDialog.askopenfile(mode='rb', filetypes=filetypes)
+    outputFile = tkFileDialog.asksaveasfile(mode='wb', filetypes=filetypes, defaultextension=".csv")
     
     joiner = joincsv.CSVRecordJoiner(inputFile)
     joiner.save(outputFile)
