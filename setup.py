06866af63184fdf913479029fcbdae94ef1f43ad
Peter Stroia-Williams <peterDwilliams@gmail.com>
2014-01-24 23:24:13 +0000

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='corow',
+      packages=['corow'],
+      version=0.1,
+      scripts=['bin/cl_corow.py', 'bin/tk_corow.py'],
+      author='Peter Stroia-Williams',
+      author_email='peterDwilliams@gmail.com',
+      url='http://github.com/peterSW/corow'
+      )
