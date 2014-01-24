d17be20b70e69224024120991da25cfd16f0350b
Peter Stroia-Williams <peterDwilliams@gmail.com>
2014-01-24 23:56:46 +0000

    Corrects version format.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name='corow',
       packages=['corow'],
-      version=0.1,
+      version='0.1',
       scripts=['bin/cl_corow.py', 'bin/tk_corow.py'],
       author='Peter Stroia-Williams',
       author_email='peterDwilliams@gmail.com',
